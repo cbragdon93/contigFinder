59dcd8ccc4c84947f1c12522dac1a6ef0b91d112
Callen Bragdon <cjoseph@bu.edu>
2021-05-17 10:53:37 -0400

    added more example cases and organized the code's formatting a bit better

--- getContigs_evaluation.py
+++ getContigs_evaluation.py
@@ -10,12 +10,24 @@ is already sorted, so no such check is currently in place
 for getContigs for sorted state of the list.
 '''
 
-# unit test
+#%%
+# unit tests
+## Case Testing
+### edge case, still contiguous at end of list
 myList = [1,  2,  4,  6,  8,  9, 10]
+print("'Contigious until EOL' edge case")
+print(getContigs(myList))
+### edge case: last element is its own contig
+myList = [1, 2, 4, 5, 6, 7, 8, 10, 11, 14, 15, 16, 17, 19, 20, 21, 22, 24, 25, 27, 29]
+print("'EOL is its own contig' edge case")
 print(getContigs(myList))
 
+
+#%%
 # generative test
+print("Generative Tests. Making random lists of the following sizes:")
 N_iter = [10, 100, 1000]
+print(N_iter)
 contigsDict = {str(n):{} for n in N_iter}
 # for reproducibility, I'd normally place a seed here.
 for n in N_iter:
@@ -26,4 +38,10 @@ for n in N_iter:
     delta = end - start
     contigsDict[str(n)] = {"time":delta, "contigs":theseContigs}
     
-print([[contigsDict[k]["time"], len(contigsDict[k]["contigs"])] for k in contigsDict.keys()])
+print([
+    [contigsDict[k]["time"], len(contigsDict[k]["contigs"])] 
+        for 
+            k 
+        in 
+            contigsDict.keys()
+])
